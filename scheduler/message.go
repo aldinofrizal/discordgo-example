7f4975ee3597b24bdd26506356be4515555ae29e
hacktiv8 <aldinofrizal@gmail.com>
2023-04-10 13:15:59 +0700

    feat: schedule and docker

--- scheduler/message.go
+++ scheduler/message.go
@@ -11,16 +11,24 @@ import (
 func InitMessageScheduler(d *discordgo.Session) {
 	s := gocron.NewScheduler(time.UTC)
 
-	// s.Every(5).Second().Do(func() {
-	// 	d.ChannelMessageSend(bot.TextGeneralId, "hello world")
-	// })
-
-	s.Every(1).Day().At("02:25").Do(func() {
-		d.ChannelMessageSend(bot.TextGeneralId, "hello world 02:23UTC")
+	s.Every(1).Day().At("01:55").Do(func() {
+		d.ChannelMessageSend(bot.TextGeneralId, "absen cuy")
 	})
 
-	s.Every(1).Day().At("21:25").Do(func() {
-		d.ChannelMessageSend(bot.TextGeneralId, "hello world 21:23WIB")
+	s.Every(1).Day().At("05:00").Do(func() {
+		d.ChannelMessageSend(bot.TextGeneralId, "istirahat siang cuy")
+	})
+
+	s.Every(1).Day().At("06:05").Do(func() {
+		d.ChannelMessageSend(bot.TextGeneralId, "beres cuy istirahat siang nya")
+	})
+
+	s.Every(1).Day().At("10:00").Do(func() {
+		d.ChannelMessageSend(bot.TextGeneralId, "balik cuy")
+	})
+
+	s.Every(1).Day().At("14:00").Do(func() {
+		d.ChannelMessageSend(bot.TextGeneralId, "tidur cuy")
 	})
 
 	s.StartAsync()
